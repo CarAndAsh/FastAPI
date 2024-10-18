eb6a926de9d5f7e8467d33960a962a798c3a04d9
CarAndAsh <zeixes@gmail.com>
2024-10-18 14:46:00 +0300

    change model User -> delete all migrations and create new;

--- Microshop/core/models/__init__.py
+++ Microshop/core/models/__init__.py
@@ -1,5 +1,6 @@
-__all__ = ('Base', 'Product', 'DatabaseHelper', 'db_helper')
+__all__ = ('Base', 'Product', 'User', 'DatabaseHelper', 'db_helper')
 
 from .base import Base
 from .db_helpers import DatabaseHelper, db_helper
 from .product import Product
+from .users import User
