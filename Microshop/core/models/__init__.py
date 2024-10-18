d643b1d568af7770a7bbf8768f4ac473755ddb2c
CarAndAsh <zeixes@gmail.com>
2024-10-18 16:43:13 +0300

    create profile for user

--- Microshop/core/models/__init__.py
+++ Microshop/core/models/__init__.py
@@ -1,7 +1,8 @@
-__all__ = ('Base', 'Product', 'User', 'Post', 'DatabaseHelper', 'db_helper')
+__all__ = ('Base', 'Product', 'User', 'Post', 'Profile', 'DatabaseHelper', 'db_helper')
 
 from .base import Base
 from .db_helpers import DatabaseHelper, db_helper
 from .product import Product
 from .users import User
 from .post import Post
+from .profile import Profile
