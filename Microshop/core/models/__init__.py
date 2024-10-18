db090345728f6212f3b5438880bb139d1a9b3a75
CarAndAsh <zeixes@gmail.com>
2024-10-18 15:28:10 +0300

    migration for posts table

--- Microshop/core/models/__init__.py
+++ Microshop/core/models/__init__.py
@@ -1,6 +1,7 @@
-__all__ = ('Base', 'Product', 'User', 'DatabaseHelper', 'db_helper')
+__all__ = ('Base', 'Product', 'User', 'Post', 'DatabaseHelper', 'db_helper')
 
 from .base import Base
 from .db_helpers import DatabaseHelper, db_helper
 from .product import Product
 from .users import User
+from .post import Post
