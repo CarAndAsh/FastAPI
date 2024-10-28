fd31eba2d38576c3d322214d3faa0d1c47811095
CarAndAsh <zeixes@gmail.com>
2024-10-28 13:44:11 +0300

    set cookie for session with demo auth api

--- Microshop/api_v1/demo_auth/views.py
+++ Microshop/api_v1/demo_auth/views.py
@@ -1,7 +1,9 @@
 import secrets
-from typing import Annotated
+import uuid
+from time import time
+from typing import Annotated, Any
 
-from fastapi import APIRouter, Depends, HTTPException, status, Header
+from fastapi import APIRouter, Depends, HTTPException, status, Header, Response, Cookie
 from fastapi.security import HTTPBasicCredentials, HTTPBasic
 
 router = APIRouter(prefix='/demo-auth', tags=['Demo Auth'])
@@ -24,6 +26,8 @@ static_auth_token_to_username = {
     'ef8d678988863e39a48b72e4e2ac52b7': 'admin',
     'f51ca4db98f9e799f313b7da3ac9cc9c': 'gin'
 }
+COOKIES: dict[str, dict[str, Any]] = {}
+COOKIE_SESSION_ID_KEY = 'web-app-session-id'
 
 
 #######################################################################################################################
@@ -57,3 +61,34 @@ def demo_basic_auth_username(auth_username: str = Depends(get_auth_user_username
 @router.get('/some-http-header-auth/')
 def demo_some_http_header_auth(username: str = Depends(get_username_by_static_auth_token)):
     return {'message': f'Nice to meet you {username}!'}
+
+
+def generate_session_id() -> str:
+    return uuid.uuid4().hex
+
+
+def get_session_data(session_id: str = Cookie(alias=COOKIE_SESSION_ID_KEY)) -> dict:
+    if session_id not in COOKIES:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='not authenticated')
+    return COOKIES[session_id]
+
+
+@router.post('/login-cookie/')
+def demo_auth_login_set_cookie(
+        response: Response,
+        # auth_username: str = Depends(get_auth_user_username),
+        auth_username: str = Depends(get_username_by_static_auth_token),
+):
+    # for login use demo_basic_auth_username or demo_some_http_header_auth
+    session_id = generate_session_id()
+    COOKIES[session_id] = {'username': auth_username, 'login_at': int(time())}
+    response.set_cookie(COOKIE_SESSION_ID_KEY, session_id)
+    return {'result': 'ok'}
+
+
+@router.get('/check-cookie/')
+def demo_auth_check_cookie(user_session_data: dict = Depends(get_session_data)):
+    return {
+        'message': f'Hello, {user_session_data["username"]}',
+        **user_session_data
+    }
