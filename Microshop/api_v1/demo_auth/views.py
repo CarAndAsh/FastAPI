f13a1f6d40b6cb3bd7459a737798eca85182bb18
CarAndAsh <zeixes@gmail.com>
2024-10-28 12:50:02 +0300

    add demo auth by token in headers api

--- Microshop/api_v1/demo_auth/views.py
+++ Microshop/api_v1/demo_auth/views.py
@@ -1,7 +1,7 @@
 import secrets
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status, Header
 from fastapi.security import HTTPBasicCredentials, HTTPBasic
 
 router = APIRouter(prefix='/demo-auth', tags=['Demo Auth'])
@@ -17,10 +17,25 @@ def demo_basic_auth_credentials(credentials: Annotated[HTTPBasicCredentials, Dep
     }
 
 
+###################################### FOR EXAMPLE. ALL SAME DATA MUST BE IN DB. ######################################
+
 username_to_password = {'admin': 'admin', 'user': 'user', 'gin': 'tonic'}
+static_auth_token_to_username = {
+    'ef8d678988863e39a48b72e4e2ac52b7': 'admin',
+    'f51ca4db98f9e799f313b7da3ac9cc9c': 'gin'
+}
 
 
-def get_auth_user_username(credentials: Annotated[HTTPBasicCredentials, Depends(security)]):
+#######################################################################################################################
+
+def get_username_by_static_auth_token(static_token: str = Header(alias='x-auth-token')) -> str:
+    if username := static_auth_token_to_username.get(static_token):
+        return username
+    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                        detail='invalid token')
+
+
+def get_auth_user_username(credentials: Annotated[HTTPBasicCredentials, Depends(security)]) -> str:
     unauthed_exc = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail='Invalid login or password',
@@ -35,7 +50,10 @@ def get_auth_user_username(credentials: Annotated[HTTPBasicCredentials, Depends(
 
 
 @router.get('/basic-auth-username/')
-def demo_basic_auth_username(
-        auth_username: str = Depends(get_auth_user_username)
-):
+def demo_basic_auth_username(auth_username: str = Depends(get_auth_user_username)):
     return {'message': f'Be happy {auth_username}!'}
+
+
+@router.get('/some-http-header-auth/')
+def demo_some_http_header_auth(username: str = Depends(get_username_by_static_auth_token)):
+    return {'message': f'Nice to meet you {username}!'}
