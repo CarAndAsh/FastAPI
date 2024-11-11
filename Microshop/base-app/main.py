2ec1668e7ffe99c188023010a79b01ba721890af
CarAndAsh <zeixes@gmail.com>
2024-11-11 15:03:54 +0300

    use run settings

--- Microshop/base-app/main.py
+++ Microshop/base-app/main.py
@@ -15,4 +15,4 @@ async def say_hello(name: str):
 
 
 if __name__ == '__main__':
-    uvicorn.run('main:app', reload=True)
+    uvicorn.run('main:app', host=settings.run.host, port=settings.run.port, reload=True)
