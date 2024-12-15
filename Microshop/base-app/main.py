3f90d22c62ebd8dfa5419f9bd4ed840779e47a35
CarAndAsh <74245305+CarAndAsh@users.noreply.github.com>
2024-12-15 19:39:07 +0300

    add db_helper dispose to main app

--- Microshop/base-app/main.py
+++ Microshop/base-app/main.py
@@ -1,22 +1,34 @@
+from contextlib import asynccontextmanager
+
 import uvicorn
 from fastapi import FastAPI
 
 from api import router as api_router
 from core.config import settings
-
-app = FastAPI()
-app.include_router(api_router, prefix=settings.api.prefix)
+from core.models import db_helper
 
 
-@app.get("/")
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # startup
+    yield
+    # shutdowm
+    await db_helper.dispose()
+
+
+main_app = FastAPI(lifespan=lifespan)
+main_app.include_router(api_router, prefix=settings.api.prefix)
+
+
+@main_app.get("/")
 async def root():
     return {"message": "Hello World"}
 
 
-@app.get("/hello/{name}")
+@main_app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
 
 
 if __name__ == '__main__':
-    uvicorn.run('main:app', host=settings.run.host, port=settings.run.port, reload=True)
+    uvicorn.run('main:main_app', host=settings.run.host, port=settings.run.port, reload=True)
