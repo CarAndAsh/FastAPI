65175c27ef5610f7ca9b942cfdc71e7dec3fae7a
CarAndAsh <74245305+CarAndAsh@users.noreply.github.com>
2024-12-18 19:30:17 +0300

    update user table

--- Microshop/base-app/core/models/user.py
+++ Microshop/base-app/core/models/user.py
@@ -1,3 +1,4 @@
+from sqlalchemy import UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column
 
 from .base import Base
@@ -6,3 +7,7 @@ from .base import Base
 class User(Base):
     __tablename__ = 'users'
     username: Mapped[str] = mapped_column(unique=True)
+    full_name: Mapped[str]
+    age: Mapped[int]
+
+    __table_args__ = UniqueConstraint('full_name', 'age'),
