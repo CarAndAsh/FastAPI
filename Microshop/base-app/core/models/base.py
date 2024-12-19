27fb1f51ee61842bca0c00c83fa0f9d430a5c176
CarAndAsh <74245305+CarAndAsh@users.noreply.github.com>
2024-12-19 15:33:36 +0300

    create int id pk mixin

--- Microshop/base-app/core/models/base.py
+++ Microshop/base-app/core/models/base.py
@@ -1,5 +1,5 @@
 from sqlalchemy import MetaData
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, declared_attr
+from sqlalchemy.orm import DeclarativeBase, declared_attr
 
 from core.config import settings
 
@@ -12,4 +12,3 @@ class Base(DeclarativeBase):
     def __tablename__(cls) -> str:
         return cls.__name__.lower()
 
-    id: Mapped[int] = mapped_column(primary_key=True)
