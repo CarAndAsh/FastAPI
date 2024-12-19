e03afb2b3ac30701234171dbdb05cf982bf3dc36
CarAndAsh <74245305+CarAndAsh@users.noreply.github.com>
2024-12-19 15:26:26 +0300

    use orjson

--- Microshop/base-app/main.py
+++ Microshop/base-app/main.py
@@ -2,6 +2,7 @@ from contextlib import asynccontextmanager
 
 import uvicorn
 from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
 
 from api import router as api_router
 from core.config import settings
@@ -19,9 +20,8 @@ async def lifespan(app: FastAPI):
     await db_helper.dispose()
 
 
-main_app = FastAPI(lifespan=lifespan)
+main_app = FastAPI(default_response_class=ORJSONResponse, lifespan=lifespan)
 main_app.include_router(api_router)
-
 
 if __name__ == '__main__':
     uvicorn.run('main:main_app', host=settings.run.host, port=settings.run.port, reload=True)
