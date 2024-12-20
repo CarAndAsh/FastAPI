2045879bc3c973a82d0bef47e00e70513bf83365
CarAndAsh <74245305+CarAndAsh@users.noreply.github.com>
2024-12-20 19:01:41 +0300

    get users db

--- Microshop/base-app/core/models/user.py
+++ Microshop/base-app/core/models/user.py
@@ -1,9 +1,18 @@
+from typing import TYPE_CHECKING
+
 from fastapi_users.db import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
 
 from .base import Base
 from .mixins.int_id_pk import IntIdPkMixin
 
+if TYPE_CHECKING:
+    from sqlalchemy.ext.asyncio import AsyncSession
+
 
 class User(Base, IntIdPkMixin, SQLAlchemyBaseUserTable[int]):
     # Inherit from Base table name as first parent
     pass
+
+    @classmethod
+    def get_db(cls, session: AsyncSession):
+        return SQLAlchemyUserDatabase(session, User)
