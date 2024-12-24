0e23a241796bc9896eeadbe5ea6a5f84a127695b
CarAndAsh <74245305+CarAndAsh@users.noreply.github.com>
2024-12-24 15:53:02 +0300

    update config: add secrets

--- Microshop/base-app/core/config.py
+++ Microshop/base-app/core/config.py
@@ -35,6 +35,8 @@ class DataBaseConfig(BaseModel):
 
 class AccessToken(BaseModel):
     lifetime_seconds: int = 3600
+    reset_password_token_secret: str
+    verification_token_secret: str
 
 
 class Settings(BaseSettings):
