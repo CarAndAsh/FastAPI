42481a19a45090b57fbdd984b1e18700dae54148
CarAndAsh <74245305+CarAndAsh@users.noreply.github.com>
2024-12-25 14:25:28 +0300

    fix UserManager token_secrets

--- Microshop/base-app/core/authentication/user_manager.py
+++ Microshop/base-app/core/authentication/user_manager.py
@@ -14,8 +14,8 @@ log = logging.getLogger(__name__)
 
 
 class UserManager(IntegerIDMixin, BaseUserManager[User, UserIdType]):
-    reset_password_token_secret = settings.reset_password_token_secret
-    verification_token_secret = settings.verification_token_secret
+    reset_password_token_secret = settings.access_token.reset_password_token_secret
+    verification_token_secret = settings.access_token.verification_token_secret
 
     async def on_after_register(self, user: User, request: Optional['Request'] = None):
         log.warning("User %r has registered.", user.id)
