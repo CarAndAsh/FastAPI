82a2cbf5d129f0c9e79ef0fbe15a24bea6f3d4ed
CarAndAsh <74245305+CarAndAsh@users.noreply.github.com>
2024-12-25 14:27:20 +0300

    include auth router from fastapi-users

--- Microshop/base-app/api/v1/auth.py
+++ Microshop/base-app/api/v1/auth.py
@@ -1,5 +1,8 @@
 from fastapi import APIRouter
 
+from api.dependencies.authentication.backend import authentication_backend
+from api.v1.fastapi_users_routers import fastapi_users
 from core.config import settings
 
 router = APIRouter(prefix=settings.api.v1.auth, tags=['Auth'])
+router.include_router(fastapi_users.get_auth_router(authentication_backend))
